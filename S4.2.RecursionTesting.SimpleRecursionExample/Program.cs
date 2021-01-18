7c404f75c9fe0331301a1d2b703918b4debec8b4
Your Name <math323n@edu.campusvejle.dk>
2021-01-18 14:30:13 +0100

    Added Factorial

--- S4.2.RecursionTesting.SimpleRecursionExample/Program.cs
+++ S4.2.RecursionTesting.SimpleRecursionExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace S4._2.RecursionTesting.SimpleRecursionExample
 {
@@ -6,15 +7,56 @@ namespace S4._2.RecursionTesting.SimpleRecursionExample
     {
         static void Main()
         {
-            Console.WriteLine(SimpleRecursion(3)); 
+            //SimpleRecursion(3);
+            //FibonacciSequence();
+
+            for(int n = 1; n <= 14; n++)
+            {
+                Console.WriteLine(FactorialRecursion(n));
+            }
+            Console.ReadLine();
+
         }
 
-       static int SimpleRecursion(int n)
+
+        private static int SimpleRecursion(int n)
         {
             if(n == 0)  // Base case
                 return -1;
             else        // Recursive case
                 return SimpleRecursion(n - 1);
         }
+
+        private static void FibonacciSequence()
+        {
+            for(int n = 2; n < 1001; n++)
+            {
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                int result = Fibonacci(n);
+                timer.Stop();
+                Console.WriteLine($"{n}, {result} Elapsed time: {timer.Elapsed}");
+                timer.Reset();
+            }
+            Console.ReadLine();
+        }
+
+
+        private static int Fibonacci(int n)
+        {
+            if(n < 2)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        private static int FactorialRecursion(int number)
+        {
+            if(number == 1)
+                return 1;
+            else
+                return number * FactorialRecursion(number - 1);
+        }
+
     }
 }
