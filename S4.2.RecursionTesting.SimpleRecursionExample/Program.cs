306a21f26384fa18f3855f4bbef1a7ed5dc83175
Your Name <math323n@edu.campusvejle.dk>
2021-01-18 13:48:28 +0100

    Add project files.

---
+++ S4.2.RecursionTesting.SimpleRecursionExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace S4._2.RecursionTesting.SimpleRecursionExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(SimpleRecursion(3)); 
+        }
+
+       static int SimpleRecursion(int n)
+        {
+            if(n == 0)  // Base case
+                return -1;
+            else        // Recursive case
+                return SimpleRecursion(n - 1);
+        }
+    }
+}
